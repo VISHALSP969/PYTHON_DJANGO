b3712c86e7c5ae2715ceeb357bf67714e7d4b250
VISHALSP969 <vishalsp969@gmail.com>
2024-09-24 19:48:21 +0530

    MyTennisClub list_display property added

--- my_tennis_club/members/models.py
+++ my_tennis_club/members/models.py
@@ -7,5 +7,5 @@ class Member(models.Model):
     phone=models.IntegerField(null=True)
     joined_date=models.DateField(null=True)
 
-    def __str__(self):
-        return f"{self.firstName} {self.lastName}"
+    # def __str__(self):
+    #     return f"{self.firstName} {self.lastName}"
