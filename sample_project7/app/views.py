f9afbdff4419c802a8150c2a905a4c4401176d03
VISHALSP969 <vishalsp969@gmail.com>
2024-09-25 12:48:10 +0530

    Template tags

---
+++ sample_project7/app/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+def testing(request):
+    template=loader.get_template('template.html')
+    context={
+        'greeting':1,
+    }
+    return HttpResponse(template.render(context,request))
