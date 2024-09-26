ea079f584fc8662f858d59a3ef987d7b0da0af27
VISHALSP969 <vishalsp969@gmail.com>
2024-09-26 12:05:30 +0530

    Operators

---
+++ sample_project10/app/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+from django.template import loader
+
+# Create your views here.
+def testing(request):
+    template=loader.get_template('template.html')
+    context={
+        'greeting':1,
+        'day':'Friday',
+        'fruits': ['Apple', 'Banana', 'Cherry'],
+        'x': ['Apple', 'Banana', 'Cherry'], 
+        'y': ['Apple', 'Banana', 'Cherry'],
+    }
+    return HttpResponse(template.render(context,request))
