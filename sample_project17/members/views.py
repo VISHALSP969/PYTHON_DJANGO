6280f5e8a8f87bf33bfb985fdae846bd2bf60262
VISHALSP969 <vishalsp969@gmail.com>
2024-09-27 15:57:07 +0530

    filter() method

--- sample_project17/members/views.py
+++ sample_project17/members/views.py
@@ -8,10 +8,12 @@ def testing(request):
     mydata=Member.objects.all()
     mydata1=Member.objects.all().values()
     mydata2=Member.objects.values_list('firstname')
+    mydata3=Member.objects.filter(firstname="Emil").values()
     template=loader.get_template('template.html')
     context={
         'mymembers':mydata,
         'mymembers1':mydata1,
         'mymembers2':mydata2,
+        'mymembers3':mydata3,
     }
     return HttpResponse(template.render(context,request))
