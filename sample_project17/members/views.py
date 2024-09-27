4b189f1a1fb6449826be4a074f2740ab81cdfdc7
VISHALSP969 <vishalsp969@gmail.com>
2024-09-27 14:25:52 +0530

    values() method

--- sample_project17/members/views.py
+++ sample_project17/members/views.py
@@ -6,8 +6,10 @@ from .models import Member
 # Create your views here.
 def testing(request):
     mydata=Member.objects.all()
+    mydata1=Member.objects.all().values()
     template=loader.get_template('template.html')
     context={
         'mymembers':mydata,
+        'mymembers1':mydata1,
     }
     return HttpResponse(template.render(context,request))
