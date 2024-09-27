fe0527b9bde6d8de4bb73035545c9091220ff9cc
VISHALSP969 <vishalsp969@gmail.com>
2024-09-27 14:49:51 +0530

    values_list() method

--- sample_project17/members/views.py
+++ sample_project17/members/views.py
@@ -7,9 +7,11 @@ from .models import Member
 def testing(request):
     mydata=Member.objects.all()
     mydata1=Member.objects.all().values()
+    mydata2=Member.objects.values_list('firstname')
     template=loader.get_template('template.html')
     context={
         'mymembers':mydata,
         'mymembers1':mydata1,
+        'mymembers2':mydata2,
     }
     return HttpResponse(template.render(context,request))
